c99ea537ceb9544d022354dfe13fc2fcddcf35f5
fumi1623 <ancient17.hirofumi@gmail.com>
2021-05-16 08:10:19 +0000

    [Add]Address

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  get 'addresses/index'
+  get 'addresses/create'
+  get 'addresses/edit'
+  get 'addresses/update'
+  get 'addresses/destroy'
   devise_for :admins
   devise_for :customers
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_15_085042) do
+ActiveRecord::Schema.define(version: 2021_05_16_054842) do
+
+  create_table "addresses", force: :cascade do |t|
+    t.integer "customer_id"
+    t.string "name"
+    t.string "postal_code"
+    t.string "address"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false
