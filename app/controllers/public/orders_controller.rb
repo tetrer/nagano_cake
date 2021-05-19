da8b599d97a933741dc6d2b3fd17a2ca1f1d4128
kazuki-koyama <kazuki.koyama92@gmail.com>
2021-05-19 07:14:26 +0000

    [add] orders#create

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -44,8 +44,30 @@ class Public::OrdersController < ApplicationController
     end
   end
 
-  def create
-  end
+	def create
+    @order = current_customer.orders.new(order_params)
+    @order.save
+    flash[:notice] = "ご注文が確定しました。"
+    redirect_to complete_orders_path
+
+    # もし情報入力でnew_addressの場合ShippingAddressに保存
+    if params[:order][:ship] == "1"
+      current_customer.addresses.create(address_params)
+    end
+
+    # カート商品の情報を注文商品に移動
+    @cart_items = current_customer.cart_items
+    @cart_items.each do |cart_item|
+      OrderDetail.create(
+        product:        cart_item.product,
+        order:          @order,
+        quantity:       cart_item.quantity,
+        subtotal_price: sub_price(cart_item)
+      )
+    end
+    # 注文完了後、カート商品を空にする
+    @cart_items.destroy_all
+	end
 
   def complete
   end
