336506893e5773799b593a25c91f73d8ae088a19
kazuki-koyama <kazuki.koyama92@gmail.com>
2021-05-19 09:48:00 +0000

    [fix] cart_itemsコントローラの不要なコメントを削除

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -1,9 +1,7 @@
 class Public::CartItemsController < ApplicationController
 
   def index
-    @cart_items = CartItem.where(customer_id: current_customer.id)   #テスト時はログインしてないのでコメントアウト中
-    # @cart_items = CartItem.where(customer_id: "1")   #テスト時用の記述（本番は削除）
-    # @cart_items = CartItem.all   #テスト時用の記述（本番は削除）
+    @cart_items = CartItem.where(customer_id: current_customer.id)
     if !@cart_items.present?
       @subtotal = 0
     else
@@ -11,24 +9,9 @@ class Public::CartItemsController < ApplicationController
         @subtotal = (@subtotal.to_i + item.product.price * item.quantity)
       end
     end
-    # @subtotal = current_customer.cart_items.products.all.sum(:price)
-    # @subtotal = @cart_items.product.sum(:price)
-
   end
 
   def create
-    # @cart_item = CartItem.new(cart_item_params)
-    # @cart_item.customer_id = current_customer_id   #テスト時はログインしてないのでコメントアウト中
-    # @cart_item.customer_id = "1"   #テスト時用の記述（本番は削除）
-    # @cart_item.quantity += params[:quantity].to_i
-    # @cart_item.save
-    # redirect_to cart_items_path
-
-    # product = Product.find(params[:product_id])
-    # cart_item = current_customer.cart_items.new(cart_item_params)
-    # cart_item.product_id = product.id
-    # cart_item.save
-
     @cart_item = CartItem.new(cart_item_params)     #空のインスタンス取得
     @cart_item.customer_id = current_customer.id     #カスタマーIDの紐付け
 
@@ -58,9 +41,7 @@ class Public::CartItemsController < ApplicationController
   end
 
   def destroy_all
-    # @cart_items = CartItem.where(customer_id: 'current_customer_id')   #テスト時はログインしてないのでコメントアウト中
-    # @cart_items = CartItem.where(customer_id: "1")   #テスト時用の記述（本番は削除）
-    @cart_items = CartItem.all
+    @cart_items = CartItem.where(customer_id: 'current_customer_id')   #テスト時はログインしてないのでコメントアウト中
     @cart_items.destroy_all
     redirect_to cart_items_path
   end
@@ -72,9 +53,4 @@ class Public::CartItemsController < ApplicationController
                                      :customer_id,
                                      :quantity)
   end
-
-  # def product_params
-  #   params.require(:product).permit(:price)
-  # end
-
 end
