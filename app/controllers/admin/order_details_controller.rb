b140e21541f01b2a90407769e279be8fe83f8613
fumi1623 <ancient17.hirofumi@gmail.com>
2021-05-20 02:45:58 +0000

    [Fix]conflict

--- app/controllers/admin/order_details_controller.rb
+++ app/controllers/admin/order_details_controller.rb
@@ -4,11 +4,7 @@ class Admin::OrderDetailsController < ApplicationController
   def update
     @order_detail = OrderDetail.find(params[:id])
     if @order_detail.update(order_detail_params)
-<<<<<<< HEAD
-      flash[:success] = "制作ステータスを変更しました"
-=======
       flash[:success] = "製作ステータスを変更しました"
->>>>>>> origin/develop
       redirect_to admin_order_path(@order_detail.order)
     else
       render "show"
@@ -18,12 +14,6 @@ class Admin::OrderDetailsController < ApplicationController
 
   private
   def order_detail_params
-<<<<<<< HEAD
-    params.require(:order_detail).permit(:order_id, :product_id, :quantity, :subtotal_price, :production_status)
-  end
-
-=======
     params.require(:order_detail).permit(:production_status)
   end
->>>>>>> origin/develop
 end
