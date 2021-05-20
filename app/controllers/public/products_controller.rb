6b47aeb76f0ca0c573f09541daf4716534261767
kazuki-koyama <kazuki.koyama92@gmail.com>
2021-05-20 15:46:22 +0000

    [update] products#index - ジャンル別に表示

--- app/controllers/public/products_controller.rb
+++ app/controllers/public/products_controller.rb
@@ -1,7 +1,11 @@
 class Public::ProductsController < ApplicationController
   def index
+    @genres = Genre.all
     @products = Product.all
-    @products = Product.page(params[:page])
+    @genre_1 = Genre.find(1)
+    @genre_2 = Genre.find(2)
+    @genre_3 = Genre.find(3)
+    @genre_4 = Genre.find(4)
   end
 
   def show
