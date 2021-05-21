85650a378085f0d6a08a800767846c5ce41e8676
fumi1623 <ancient17.hirofumi@gmail.com>
2021-05-21 06:26:07 +0000

    [Add]rspec

--- spec/models/customer_spec.rb
+++ spec/models/customer_spec.rb
@@ -1,5 +1,27 @@
 require 'rails_helper'
 
-RSpec.describe Customer, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+RSpec.describe 'customerのモデルテスト', type: :model do
+
+
+  describe 'バリデーションのテスト' do
+    subject { customer.valid? }
+    let!(:other_customer) { create(:customer) }
+    let(:customer) { build(:customer) }
+
+    context 'last_nameカラム' do
+      it '空欄でないこと' do
+        customer.last_name = ''
+        is_expected.to eq false
+      end
+    end
+
+    context 'first_nameカラム' do
+      it '空欄でないこと' do
+        customer.first_name = ''
+        is_expected.to eq false
+      end
+    end
+
+
+  end
 end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -31,6 +31,11 @@ rescue ActiveRecord::PendingMigrationError => e
   exit 1
 end
 RSpec.configure do |config|
+
+  config.include Devise::Test::IntegrationHelpers, type: :request #sign_inヘルパーを提供してくれます
+  config.include FactoryBot::Syntax::Methods #ついでにFactoryBotもincludeしておきます
+
+
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
 
