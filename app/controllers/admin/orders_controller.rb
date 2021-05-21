b717017bf8b2f11c2860df6a6062e325d855df73
kazuki-koyama <kazuki.koyama92@gmail.com>
2021-05-21 07:22:31 +0000

    [update] admin/ordersコントローラ - ステータス更新の連動

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -2,8 +2,6 @@ class Admin::OrdersController < ApplicationController
   before_action :authenticate_admin!
   include ApplicationHelper
 
-  before_action :authenticate_admin!
-
   def index
     @orders = Order.all.page(params[:page]).per(10)
   end
@@ -15,15 +13,19 @@ class Admin::OrdersController < ApplicationController
 
   def update
     @order = Order.find(params[:id])
-    if @order.update(order_params)
-      redirect_to admin_order_path(@order)
+    @order.update(order_params)
+    @order_details = @order.order_details
+    if @order.order_status == "入金確認"
+      @order_details.each do |order_detail|
+        order_detail.update(production_status: 1)
+      end
     end
+    redirect_to admin_order_path(@order)
   end
 
   private
   def order_params
     params.require(:order).permit(:order_status)
   end
-
 end
 
