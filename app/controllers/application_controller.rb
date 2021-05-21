fa4ece0fe377687c8fcae4e8396c8a9fcb30bae8
kazuki-koyama <kazuki.koyama92@gmail.com>
2021-05-21 01:37:17 +0000

    [update] public/products#index - 商品情報をpartial化

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,16 +1,11 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
-  before_action :get_genres, if: proc { !admin_signed_in? }
 
   protected
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up,
       keys: [:first_name, :last_name, :kana_first_name, :kana_last_name,
              :email, :postal_code, :address, :phone_number])
-  end
-
-  def get_genres
-    @genres = Genre.all
   end
 
 end
