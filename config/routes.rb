5bd6ec711ab6764f1a5761fdc34d35e919507d82
Ryo-exid <sakura4oneok@gmail.com>
2021-05-21 04:24:36 +0000

    [Fix]routes

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ Rails.application.routes.draw do
     resources :genres, only: [:index, :create, :edit, :update]
     resources :orders, only: [:index, :show, :update]
     resources :order_details, only: [:update]
+    resources :customers, only: [:index, :show, :edit, :update]
   end
 
   scope module: :public do
