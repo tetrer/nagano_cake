a15c50b34fd3a7430cec2ac8ffc4f2bc6b960e33
Ryo-exid <sakura4oneok@gmail.com>
2021-05-22 13:07:59 +0000

    [Add]product_spec.rb,order_spec.rb,order_detail_spec.rbそれぞれに記述

--- spec/models/order_detail_spec.rb
+++ spec/models/order_detail_spec.rb
@@ -1,5 +1,27 @@
-# require 'rails_helper'
+require 'rails_helper'
 
-# RSpec.describe OrderDetail, type: :model do
-#   pending "add some examples to (or delete) #{__FILE__}"
-# end
+RSpec.describe 'OrderDetailのテスト', type: :model do
+  describe 'バリデーションのテスト' do
+    subject { order_detail.valid? }
+
+    let(:order) { create(:order) }
+    let(:product) { create(:product) }
+    let!(:order_detail) { build(:order_detail, order_id: order.id) }
+    let!(:order_detail) { build(:order_detail, product_id: product.id) }
+
+  end
+
+  describe 'アソシエーションのテスト' do
+    context 'Orderモデルとの関係' do
+      it 'N:1となっている' do
+        expect(OrderDetail.reflect_on_association(:order).macro).to eq :belongs_to
+      end
+    end
+
+    context 'Productモデルとの関係' do
+      it 'N:1となっている' do
+        expect(OrderDetail.reflect_on_association(:product).macro).to eq :belongs_to
+      end
+    end
+  end
+end
