51b130a87545ffa71ad3b564d9c11136a79c561c
fumi1623 <ancient17.hirofumi@gmail.com>
2021-05-23 13:15:45 +0000

    [Update]rspec_cartItem

--- spec/system/system_cartItem_spec.rb
+++ spec/system/system_cartItem_spec.rb
@@ -56,10 +56,9 @@ describe 'cartItem関連' do
       @genre = create(:genre)
       @product1 = create(:product, genre: @genre)
       @product2 = create(:product, genre: @genre)
-      # @cartItem = create(:cartItem, customer: @customer)
       visit product_path(@product1)
     end
-    context 'product1をカートに入れる' do
+    context 'productそれぞれをカートに入れる' do
       it 'product1を１個カートに入れる：カートに移動してproduct1が１個追加される' do
         select(value = 1, from: 'cart_item[quantity]')
         click_button 'カートに入れる'
@@ -68,13 +67,53 @@ describe 'cartItem関連' do
         expect(page).to have_content ((@product1.price * 1.1).floor).to_s(:delimited)
         find("option[value='1']").select_option
       end
+      it '既に別の商品が入っていても別の商品が追加される：合計金額が正確に反映される' do
+        select(value = 1, from: 'cart_item[quantity]')
+        click_button 'カートに入れる'
+        visit product_path(@product2)
+        select(value = 3, from: 'cart_item[quantity]')
+        click_button 'カートに入れる'
+        expect(current_path).to eq cart_items_path
+        expect(page).to have_content @product1.name
+        expect(page).to have_content @product2.name
+        expect(page).to have_content @subtotal
+      end
+      it '同じ商品が入った場合、合算される' do
+        select(value = 1, from: 'cart_item[quantity]')
+        click_button 'カートに入れる'
+        visit product_path(@product1)
+        select(value = 1, from: 'cart_item[quantity]')
+        click_button 'カートに入れる'
+        find("option[value='3']").select_option
+      end
     end
-
-
-
   end
 
+  describe '削除機能' do
+    before do
+      @genre = create(:genre)
+      @product1 = create(:product, genre: @genre)
+      @product2 = create(:product, genre: @genre)
+      visit product_path(@product1)
+      select(value = 1, from: 'cart_item[quantity]')
+      click_button 'カートに入れる'
+    end
 
-
-
+    context 'カートに入れた商品を削除できる' do
+      it '個別で削除する' do
+        click_link 'X 削除する'
+        expect(page).not_to have_content @product1.name
+        expect(page).to have_content 'カートの中身がありません'
+      end
+      it '複数の商品を一括で削除する' do
+        visit product_path(@product2)
+        select(value = 1, from: 'cart_item[quantity]')
+        click_button 'カートに入れる'
+        click_link 'カートを空にする'
+        expect(page).not_to have_content @product1.name
+        expect(page).not_to have_content @product2.name
+        expect(page).to have_content 'カートの中身がありません'
+      end
+    end
+  end
 end
