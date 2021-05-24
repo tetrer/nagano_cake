b7bd06ac97017674cac0ad9900802cbe1c864c9b
rui-21 <reisu6235@gmail.com>
2021-05-24 07:13:27 +0000

    [Add]genreモデルテスト、表示内容のテスト追加

--- spec/models/genre_spec.rb
+++ spec/models/genre_spec.rb
@@ -3,6 +3,12 @@ require 'rails_helper'
 RSpec.describe 'genreのモデルテスト', type: :model do
   describe 'バリデーションのテスト' do
     subject { genre.valid? }
-    let!()
+    let(:genre) {create(:genre)}
+    context 'nameカラム' do
+      it '空欄でないこと' do
+        genre.name = ''
+        is_expected.to eq false
+      end
+    end
   end
 end
--- spec/system/system_genre_spec.rb
+++ spec/system/system_genre_spec.rb
@@ -14,13 +14,23 @@ describe 'ジャンル関連' do
   end
 
   describe '表示内容の確認' do
+
     context '新規登録フォーム' do
       it 'ジャンル名' do
         expect(page).to have_field 'genre[name]'
       end
+      it '新規登録ボタンがある' do
+        expect(page).to have_button '新規登録'
+      end
     end
+
     context '一覧' do
-      it 
+      it 'ジャンル名' do
+        expect(page).to have_content @genre.name
+      end
+      it '編集リンク' do
+        expect(page).to have_link '編集する'
+      end
     end
   end
 
