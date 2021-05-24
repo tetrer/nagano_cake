ebc68110fd2401170cdfc2bb629a33dda41e8a2b
kazuki-koyama <kazuki.koyama92@gmail.com>
2021-05-24 14:12:52 +0000

    [add] ordersコントローラのテスト

--- spec/factories/order_details.rb
+++ spec/factories/order_details.rb
@@ -1,11 +1,9 @@
 FactoryBot.define do
-  factory :order do
-    total_price { "2340" }
-    payment_method { "クレジットカード" }
-    name { Gimei.name }
-    postal_code { '0000000' }
-    address { '東京のどこかに住んでるよ' }
-    order_status { "入金待ち" }
+  factory :order_detail do
+    quantity { 5 }
+    subtotal_price { 2340 }
+    production_status { 0 }
     customer
+    order
   end
 end
